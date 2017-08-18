fdb0511b69c200b7513799aca811ecafe0a368df
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-08-17 21:57:39 -0800

    Json server and Axios added.

--- schema.js
+++ schema.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,12 +9,14 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
+/*
 // Hardcoded data
 const customers = [
   {id:'1', name:'John Doe', email:'jdoe@gmail.com', age:'35'},
   {id:'2', name:'Steve Smith', email:'ssmith@gmail.com', age:'32'},
   {id:'3', name:'Sara Williams', email:'swilliams@gmail.com', age:'25'},
 ]
+*/
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -35,17 +39,19 @@ const RootQuery = new GraphQLObjectType({
         id: {type:GraphQLString},
       },
       resolve(parentValue, args){
+        /*
         for(let i = 0; i< customers.length; i++){
           if(customers[i].id == args.id){
             return customers[i];
           }
-        }
+        }*/
+        return axios.get('http://localhost:3000/customers/'+args.id).then(res => res.data);
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args){
-        return customers;
+        return axios.get('http://localhost:3000/customers/').then(res => res.data);
       }
     }
   }
